f4bd211d2990381d06d0b177ad1cff473bc304f8
Mashood Murtaza <mashood.murtaza@gmail.com>
2025-08-02 14:26:24 +0500

    added model, http client

---
+++ CombineWithSwiftUI/Models/Movie.swift
@@ -0,0 +1,31 @@
+//
+//  Movie.swift
+//  CombineWithSwiftUI
+//
+//  Created by Mashud Murtaza on 02/08/2025.
+//
+
+import Foundation
+
+struct MovieResponse: Decodable {
+    let Search: [Movie]
+}
+
+struct Movie: Identifiable, Decodable {
+    
+    let title: String
+    let year: String
+    let imdbId: String
+    let poster: URL?
+    
+    var id: String {
+        imdbId
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case year = "Year"
+        case imdbId = "imdbID"
+        case poster = "Poster"
+    }
+}
