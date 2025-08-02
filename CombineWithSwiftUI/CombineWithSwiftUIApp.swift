bb11d0dc580f876410ec1eeeda2b60d33c6552e0
Mashood Murtaza <mashood.murtaza@gmail.com>
2025-08-02 14:24:12 +0500

    first commit

---
+++ CombineWithSwiftUI/CombineWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineWithSwiftUIApp.swift
+//  CombineWithSwiftUI
+//
+//  Created by Mashud Murtaza on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CombineWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
